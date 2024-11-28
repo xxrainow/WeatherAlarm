14abca23a2b5b31894b083a669deaa7b8d56da8d
xxrainow <sjw1770@seoultech.ac.kr>
2024-11-28 12:43:36 +0900

    add icon 'imageWeather'

--- app/src/main/java/com/example/weatherapp/adapter/WeatherAdapter.java
+++ app/src/main/java/com/example/weatherapp/adapter/WeatherAdapter.java
@@ -3,6 +3,7 @@ package com.example.weatherapp.adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -45,24 +46,19 @@ public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.ViewHold
         }
 
         public void setItem(ModelWeather item) {
+            ImageView imgWeather = itemView.findViewById(R.id.imgWeather);
             TextView tvTime = itemView.findViewById(R.id.tvTime);           // 시각
             TextView tvRainType = itemView.findViewById(R.id.tvRainType);   // 강수 형태
             TextView tvHumidity = itemView.findViewById(R.id.tvHumidity);   // 습도
             TextView tvSky = itemView.findViewById(R.id.tvSky);             // 하늘 상태
             TextView tvTemp = itemView.findViewById(R.id.tvTemp);           // 온도
-            TextView tvRecommends = itemView.findViewById(R.id.tvRecommends); // 옷 추천
 
+            imgWeather.setImageResource(getWeatherImage(item.getSky()));
             tvTime.setText(item.getFcstTime());
             tvRainType.setText(getRainType(item.getRainType()));
             tvHumidity.setText(item.getHumidity());
             tvSky.setText(getSky(item.getSky()));
             tvTemp.setText(item.getTemp() + "°");
-
-            try {
-                tvRecommends.setText(getRecommends(Integer.parseInt(item.getTemp())));
-            } catch (NumberFormatException e) {
-                tvRecommends.setText("온도 데이터 오류");
-            }
         }
     }
 
@@ -96,24 +92,17 @@ public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.ViewHold
         }
     }
 
-    // 옷 추천
-    private String getRecommends(int temp) {
-        if (temp >= 5 && temp <= 8) {
-            return "울 코트, 가죽 옷, 기모";
-        } else if (temp >= 9 && temp <= 11) {
-            return "트렌치 코트, 야상, 점퍼";
-        } else if (temp >= 12 && temp <= 16) {
-            return "자켓, 가디건, 청자켓";
-        } else if (temp >= 17 && temp <= 19) {
-            return "니트, 맨투맨, 후드, 긴바지";
-        } else if (temp >= 20 && temp <= 22) {
-            return "블라우스, 긴팔 티, 슬랙스";
-        } else if (temp >= 23 && temp <= 27) {
-            return "얇은 셔츠, 반바지, 면바지";
-        } else if (temp >= 28 && temp <= 50) {
-            return "민소매, 반바지, 린넨 옷";
-        } else {
-            return "패딩, 누빔 옷, 목도리";
+    private int getWeatherImage(String sky) {
+        // 하늘 상태에 따라 이미지 반환
+        switch (sky) {
+            case "1": // 맑음
+                return R.drawable.sun_cloud;
+            case "3": // 구름 많음
+                return R.drawable.very_cloudy;
+            case "4": // 흐림
+                return R.drawable.cloudy;
+            default: // 오류
+                return R.drawable.ic_launcher_foreground;
         }
     }
 }
